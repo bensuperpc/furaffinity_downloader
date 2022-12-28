ab2591eae54fe12f44fd530595c265005be1205a
Bensuperpc <bensuperpc@gmail.com>
2022-12-28 17:35:11 +0100

    Update downloader with sub folder for each artist
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- furaffinity.py
+++ furaffinity.py
@@ -16,27 +16,17 @@ import faapi
 
 class FurAffinity:
     def __init__(self, **kwargs):
+        logger.info("Initializing FurAffinity")
 
-        kwargs.setdefault("cookies", None)
-        kwargs.setdefault("a_cookies", None)
-        kwargs.setdefault("b_cookies", None)
-        kwargs.setdefault("thread_worker", 32)
-        kwargs.setdefault("output_dir", None)
+        self.set_cookies(kwargs.get("cookies", None),
+                         kwargs.get("a_cookies", None), kwargs.get("b_cookies", None))
+
+        #kwargs.setdefault("thread_worker", 32)
+        self.thread_worker = kwargs.get("thread_worker", 32)
+        self.output_dir = kwargs.get("output_dir", os.getcwd())
 
         # for key, val in kwargs.items():
         #    self.__dict__[key] = val
-
-        logger.info("Initializing FurAffinity")
-
-        self.thread_worker = kwargs["thread_worker"]
-
-        if kwargs["output_dir"] is None:
-            self.output_dir = os.getcwd()
-        else:
-            self.output_dir = kwargs["output_dir"]
-
-        self.set_cookies(kwargs["cookies"],
-                         kwargs["a_cookies"], kwargs["b_cookies"])
         logger.info("FurAffinity initialized")
 
     # Define cookies for the session
@@ -50,21 +40,19 @@ class FurAffinity:
             logger.info("Using provided cookies")
             self.cookies = cookies
             self.api = faapi.FAAPI(self.cookies)
-
         elif a_cookies and b_cookies:
             logger.info("Using provided a and b cookies")
             self.cookies = RequestsCookieJar()
             self.cookies.set("a", a_cookies)
             self.cookies.set("b", b_cookies)
             self.api = faapi.FAAPI(self.cookies)
-
         else:
             logger.warning("No cookies provided, using default")
             self.cookies = RequestsCookieJar()
             self.api = faapi.FAAPI(self.cookies)
 
     # Download submissions from ID
-    def download_submission(self, subID: int, wait_time: bool = True, retry_time: int = 5, retry_count: int = 3):
+    def download_submission(self, subID: int, wait_time: bool = True, retry_time: int = 5, retry_count: int = 1):
 
         # To avoid getting errors from the server due to massive downloading, we wait a random amount of time (100-10000ms)
         if wait_time:
@@ -82,15 +70,27 @@ class FurAffinity:
                 logger.debug(
                     f"Downloading {sub.id} {sub.title} {sub.author} {(len(sub_file) / 1024):.3f}KiB")
 
-                output_file = os.path.join(
-                    self.output_dir, sub.file_url.split('/')[-1])
-                logger.debug(f"Writting {output_file}")
+                file_name = str(sub.file_url.split("/")[-1])
+                artist_name = str(sub.author)
+                submission_type = str(sub.type)
 
-                # os.makedirs(self.output_dir, exist_ok=True)
+                # Check if artist folder does not exist, create it
+                if not os.path.exists(os.path.join(self.output_dir, artist_name)):
+                    os.makedirs(os.path.join(self.output_dir, artist_name))
 
-                with open(sub.file_url.split("/")[-1], "wb") as f:
-                    f.write(sub_file)
+                # Check if submission type folder does not exist, create it
+                if not os.path.exists(os.path.join(self.output_dir, artist_name, submission_type)):
+                    os.makedirs(os.path.join(self.output_dir,
+                                artist_name, submission_type))
 
+                # Check if submission file does not exist, create it
+                if not os.path.exists(os.path.join(self.output_dir, artist_name, submission_type, file_name)):
+                    with open(os.path.join(self.output_dir, artist_name, submission_type, file_name), "wb") as f:
+                        f.write(sub_file)
+                    logger.success(
+                        f"Successfully downloaded and saved {subID}")
+                else:
+                    logger.debug(f"File {file_name} already exists, skipping")
                 break
             except Exception as e:
                 logger.error(e)
@@ -105,7 +105,7 @@ class FurAffinity:
             return
 
     # Get submission IDs from artist from gallery
-    def get_all_gallery_submission_id(self, user):
+    def get_all_gallery_submission_id(self, user: str):
         submissionIDs = []
 
         logger.info(f"Getting submission IDs of {user} from gallery")
@@ -121,7 +121,7 @@ class FurAffinity:
         return submissionIDs
 
     # Get submission IDs from artist from scraps
-    def get_all_scraps_submission_id(self, user):
+    def get_all_scraps_submission_id(self, user: str):
         submissionIDs = []
 
         logger.info(f"Getting submission IDs of {user} from scraps")
@@ -162,7 +162,7 @@ class FurAffinity:
 
         return results
 
-    def download_gallery(self, user):
+    def download_gallery(self, user: str):
 
         logger.info(f"Downloading gallery of {user}")
 
@@ -191,7 +191,9 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--artists', nargs='+',
                         default=[], help='Artists to download from')
     parser.add_argument('-j', '--jobs', type=int, default=32,
-                        help="Number of parallel downloads")
+                        help="Number of parallel downloads, by default it is 32")
+    parser.add_argument('-o', '--output', default=os.getcwd(),
+                        help="Output directory, by default it is the current directory")
 
     args = parser.parse_args()
     artists = args.artists
@@ -204,7 +206,7 @@ if __name__ == "__main__":
     logger.debug(f"Using {args.jobs} jobs")
 
     furAffinity = FurAffinity(
-        a_cookies=COOKIE_A, b_cookies=COOKIE_B, thread_worker=args.jobs)
+        a_cookies=COOKIE_A, b_cookies=COOKIE_B, thread_worker=args.jobs, output_dir=args.output)
 
     for artist in artists:
         furAffinity.download_gallery(artist)
